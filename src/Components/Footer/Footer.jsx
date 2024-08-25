ff779d136159ed9967de1f493cb3605e7ec705fe
Shubhamashish1310 <98949989+Shubhamashish1310@users.noreply.github.com>
2024-08-26 01:33:58 +0530

    add

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     <footer className="fixed bottom-0 left-0 footer p-4 bg-base-200 text-base-content rounded-b flex justify-center items-center">
       <p className="text-center">
         {new Date().getFullYear()} - Made with ❤️ by Shubham Ashish 
-        <a href="https://www.linkedin.com/in/shubhamashish/" className="text-blue-500 text-lg font-bold hover:underline"> Shubham Ashish</a>
+        <a href="https://www.linkedin.com/in/shubhamashish/" className="text-blue-500 text-lg font-bold hover:underline"> Shubham Ashish </a>
       </p>
     </footer>
   )
